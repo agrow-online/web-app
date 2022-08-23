fd70ed0c54a7ca5219f3d3772f93b25c6112642c
winnekes <simona.winnekes@googlemail.com>
2022-08-23 15:51:35 +0200

    feat(ui): updat app font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Figtree:wght@500;700&display=swap"
           rel="stylesheet"
         />
       </Head>
