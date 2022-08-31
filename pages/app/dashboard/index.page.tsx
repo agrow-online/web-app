165c0d3b55a38f3b22563aaacbb92f8305b6c2de
winnekes <simona.winnekes@googlemail.com>
2022-08-31 12:46:15 +0200

    feat(ux): redirect authenticated user to app entry

--- pages/app/dashboard/index.page.tsx
+++ pages/app/dashboard/index.page.tsx
@@ -1,6 +1,10 @@
+import { useUser } from '@supabase/auth-helpers-react';
 import { NextPage } from 'next';
 
 const DashboardPage: NextPage = () => {
+  const t = useUser();
+
+  console.log(t);
   return <div>Your dashboard</div>;
 };
 
