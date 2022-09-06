0a84bedd5cd6f5b4476cde83621759b8aeee076b
winnekes <simona.winnekes@googlemail.com>
2022-09-06 22:09:44 +0200

    feat(dev): add chakra for development & accessibility

---
+++ theme/theme.ts
@@ -0,0 +1,8 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const fonts = {
+  heading: `'Figtree', sans-serif`,
+  body: `'Figtree', sans-serif`,
+};
+
+export const theme = extendTheme({ fonts });
